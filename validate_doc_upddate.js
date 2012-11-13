6f0d2b6cfbad5a376cafa06b38ce898b001102c5
Kevin Gaudin <kevin.gaudin@gmail.com>
2012-11-13 23:34:07 +0100

    Require reporter role to put documents

--- validate_doc_upddate.js
+++ validate_doc_upddate.js
@@ -1,5 +1,5 @@
 function(newDoc, oldDoc, userCtx) {
 	if(userCtx.roles.indexOf("reporter") < 0) {
-		throw({"forbidden": "You may only update documents with reporter role "});
+		throw({"forbidden": "You may only post reports with reporter role "});
 	}
 }
