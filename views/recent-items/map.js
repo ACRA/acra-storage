e209031c09d3bf6cdcc314fba26c914818922f40
Kevin Gaudin <kevin.gaudin@gmail.com>
2012-12-28 00:32:47 +0100

    Adding stack trace to help determine a signature. Next step would be to compute the signature when updating a report.

--- views/recent-items/map.js
+++ views/recent-items/map.js
@@ -3,7 +3,8 @@ function(doc) {
       emit(new Date(doc.USER_CRASH_DATE), {
           user_crash_date: doc.USER_CRASH_DATE,
           stack_trace:doc.STACK_TRACE,
-          android_version : doc.ANDROID_VERSION
+          android_version : doc.ANDROID_VERSION,
+          application_package : doc.APPLICATION_PACKAGE
       });
   }
 };
