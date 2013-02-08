2bd6aa09dcccac4714a37f08f935efa5bdf67895
Kevin Gaudin <kevin.gaudin@gmail.com>
2013-02-09 00:20:44 +0100

    Real world RSS

--- lists/rss.js
+++ lists/rss.js
@@ -2,14 +2,34 @@ function (head, req) {
     send('<rss version="0.91">');
 
     send('<channel>');
-    send('<title>The Smallest test channel</title>');
-    send('<link>http://www.thesmallest.com/</link>');
-    send('<description>Example channel construction test</description>');
+    send('<title>Latest Crash Reports</title>');
+    send('<link>http://' + req.headers.Host + '/' + req.path[0] + '/' + '_design/acralyzer/index.html</link>');
+    send('<description>Acralyzer latest crash reports.</description>');
     while (row = getRow()) {
         send('<item>');
-        send('<title>New lessonette</title>');
-        send('<link>http://www.thesmallest.com/lessonettes/</link>');
-        send('<description>The Smallest publishes a new lesson</description>');
+        send('<title>');
+        if(row.value.signature) {
+            send(row.value.signature.digest);
+        }
+        send('</title>');
+        send('<link>http://'+ req.headers.Host);
+        send('/' + req.path[0] + '/' + '_design/acralyzer/index.html#/ReportDetails?id=' + row.id +'</link>');
+        send('<description>');
+        if(row.value.application_version_name) {
+            send('<p>app_version: ' + row.value.application_version_name + '</p>');
+        }
+        if(row.value.android_version) {
+            send('<p>android_version: ' + row.value.android_version + '</p>');
+        }
+        if(row.value.device) {
+            send('<p>device: ' + row.value.device + '</p>');
+        }
+        if(row.value.signature) {
+            send('<p>crash line: ' + row.value.signature.full + '</p>');
+        }
+        send('</description>');
+        send('<guid>' + row.id + '</guid>');
+        send('<pubDate>' + row.key + '</pubDate>')
         send('</item>');
     }
     send('</channel>');
